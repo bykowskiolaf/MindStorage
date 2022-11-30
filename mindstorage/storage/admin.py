e52a51dc07fb3b16804e6e3f1d2302b2f6018530
laxmymow <80284723+laxmymow@users.noreply.github.com>
2022-11-30 16:37:55 +0100

    Delete mindstorage directory

--- mindstorage/storage/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Plik
-
-@admin.register(Plik)
-class Plik(admin.ModelAdmin):
-    pass
--- mindstorage/storage/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-
-from .models import Plik
-
-class Upload(ModelForm):
-    class Meta:
-        model = Plik
-        fields = ['plik']
